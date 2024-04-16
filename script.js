e186b8c843acbdc7d896d46f7961c686b9cf88ec
Saksham-Batra <140792075+Saksham-Batra@users.noreply.github.com>
2024-04-16 19:22:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded' , function() {
+	const inputs = document.querySelectorAll('.code');
+	inputs.forEach((input,index) => {
+		input.addEventListener('input', function() {
+			if(this.value.length>=1 && index< inputs.length-1){
+				inputs[index+1].focus();
+			}
+		});
+		input.addEventListener('keydown', function (e) {
+			if(e.key === 'Backspace' && this.value.length === 0 && index>0) {
+				inputs[index-1].focus();
+			}
+		});
+	});
+});
